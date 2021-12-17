a5dabf3c2da3e2db4a4bf05aa13a66c6007b3bc2
Jinneon <Jinneon@users.noreply.github.com>
2021-12-17 11:49:30 +0200

    Coded feature. File Added.txt has all commit details

--- src/AgileGroupwork/Program.cs
+++ src/AgileGroupwork/Program.cs
@@ -25,30 +25,72 @@ namespace Groupwork
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             command.ExecuteNonQuery();
 
-            // Insert dummy data
-            sql = "INSERT INTO Highscores (name, score) VALUES ('Me', 9001)";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-            sql = "INSERT INTO Highscores (name, score) VALUES ('Myself', 6000)";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-            sql = "INSERT INTO Highscores (name, score) VALUES ('And I', 9001)";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-
-
-
-            // Read the data from the database
-            sql = "SELECT * FROM Highscores ORDER BY score DESC";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            SQLiteDataReader reader = command.ExecuteReader();
-            while (reader.Read())
+            Console.WriteLine("How many players?");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            while (true)
             {
-                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+                if (count < input)
+                {
+                    Console.WriteLine("Player name?");
+                    string personName = Console.ReadLine();
+
+                    sql = "INSERT INTO Highscores (name, score) " +
+              "VALUES (@someValue, @someOtherValue);";
+                    int defaultScore = 0;
+
+                    using (var cmd = new SQLiteCommand(sql, m_dbConnection))
+                    {
+                        cmd.Parameters.AddWithValue("@someValue", personName);
+                        //Default value for score is zero;
+                        cmd.Parameters.AddWithValue("@someOtherValue", defaultScore);
+                        cmd.ExecuteNonQuery();
+                    }
+                    count++;
+
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            Console.WriteLine("Add score for a person: Write 1");
+            Console.WriteLine("Show all scores: Write 2");
+            Console.WriteLine("Show a persons score: Write 3");
+            //  Console.WriteLine("Extras options: Write ex");
+            Console.WriteLine("Add score for a person: Write quit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 2)
+            {
+                Console.WriteLine("");
+                sql = "SELECT * FROM Highscores ORDER BY score DESC";
+                command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+                }
+
+            }
+            else if (choice == 1)
+            {
+                //Add feature add score for a person
             }
 
-            // Close the connection
+            // Insert dummy data
+            /*  sql = "INSERT INTO Highscores (name, score) VALUES ('Me', 9001)";
+              command = new SQLiteCommand(sql, m_dbConnection);
+              command.ExecuteNonQuery();
+              sql = "INSERT INTO Highscores (name, score) VALUES ('Myself', 6000)";
+              command = new SQLiteCommand(sql, m_dbConnection);
+              command.ExecuteNonQuery();
+              sql = "INSERT INTO Highscores (name, score) VALUES ('And I', 9001)";
+              command = new SQLiteCommand(sql, m_dbConnection);
+              command.ExecuteNonQuery();*/
             m_dbConnection.Close();
         }
+
+
     }
 }
